592f72bbadfc2ed8b7d4a02d31f70c52e63334fb
2308Bohdan <63585465+2308Bohdan@users.noreply.github.com>
2021-09-01 13:38:51 +0300

    Delete script.js

--- 6) Массивы/script.js
+++
@@ -1,8 +0,0 @@
-var someArray = [];
-someArray[0] = 28;
-someArray[1] = "some string";
-someArray[2] = false;
-someArray[3] = null;
-alert(someArray.length);
-someArray[4] = prompt("Введите значание", "");
-alert(someArray[4]);
